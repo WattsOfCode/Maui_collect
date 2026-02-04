3ed8cba76815433b226285b5b16b91d091ec603e
WattsOfCode <johnddeardorff@gmail.com>
2026-02-03 22:41:58 -0500

    Initial commit for 2nd practice of MAUI

---
+++ MAUI APP 2/MauiApp2/MauiApp2/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace MauiApp2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnDisplayClicked(object sender, EventArgs e)
+        {
+            lblOutput.Text = txtInput.Text;
+        }
+
+        private void OnResetClicked(object sender, EventArgs e)
+        {
+            txtInput.Text = string.Empty;
+            lblOutput.Text = string.Empty;
+        }
+    }
+}
