aece0f8bfcaf0d65b49f01bf55a745391d9b8641
WattsOfCode <johnddeardorff@gmail.com>
2026-02-08 16:51:31 -0500

    Project check point 0.02

--- MAUI APP 2/Project files/RSVP Application/RSVP Application/EventListPage.xaml.cs
+++ MAUI APP 2/Project files/RSVP Application/RSVP Application/EventListPage.xaml.cs
@@ -2,8 +2,20 @@ namespace RSVP_Application;
 
 public partial class EventListPage : ContentPage
 {
-	public EventListPage()
+	async void OnEventSelected(object sender, SelectionChangedEventArgs e)
 	{
-		InitializeComponent();
-	}
+        if (e.CurrentSelection.FirstOrDefault() is Event selectedEvent)
+        {
+            await Navigation.PushAsync(new EventPage(selectedEvent));
+        }
+    ((CollectionView)sender).SelectedItem = null;
+    }
+
+    //Navigating to the Create Event flow
+    async void OnCreateEvent_Clicked(object sender, EventArgs e)
+    {
+        // Navigate to the creation page 
+        await Navigation.PushAsync(new CreateEventPage());
+    
+    }
 }
--- MAUI APP 2/Project files/RSVP Application/RSVP Application/EventPage.xaml.cs
+++ MAUI APP 2/Project files/RSVP Application/RSVP Application/EventPage.xaml.cs
@@ -2,8 +2,24 @@ namespace RSVP_Application;
 
 public partial class EventPage : ContentPage
 {
-	public EventPage()
+	bool IsCreator = false;
+
+	public EventPage(bool userIsCreator)
 	{
 		InitializeComponent();
-	}
+		IsCreator = userIsCreator;
+
+		//toggle visibility
+		CreatorSection.IsVisible = IsCreator;
+		GuestSection.IsVisible = !IsCreator;
+
+		//setting fields to read only if not creator
+		EventNameEntry.IsReadOnly = !IsCreator;
+		EventLocationEntry.IsReadOnly = !IsCreator;
+		EventDescriptionEditor.IsReadOnly = !IsCreator;
+
+		//date picker settings
+		EventDatePicker.IsEnabled = IsCreator;
+		EventTimePicker.IsEnabled = IsCreator;
+    }
 }
